292b2c5f5089774c8c56b1a43980564e79176f15
Bull1303 <2lp2@mail.ru>
2024-02-10 16:28:12 +0300

    Initial commit

---
+++ store/send_mails.py
@@ -0,0 +1,29 @@
+from django.core.mail import send_mail
+import os
+import environ
+
+env = environ.Env()
+
+EMAIL = env('EMAIL_HOST_USER')
+
+SUBJECTS = [
+    'Registration confirmation',
+    'Password reset',
+    'Order confirmation'
+]
+
+MESSAGES = [
+    'New user has been registered successfully',
+    'Your password has been reset successfully',
+    'Dear customer, thank you for order. Our manager will contact you soon to confirm order details'
+]
+
+
+def send_custom_mail(user, subject, message):
+    return send_mail(
+        subject=subject,
+        message=message,
+        from_email=EMAIL,
+        recipient_list=[user.email],
+        fail_silently=False,
+    )
